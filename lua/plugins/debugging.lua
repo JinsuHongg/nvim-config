de6ff6e517172a4f0a29c1440ad4f85a070bde39
Jinsu Hong <jinsuhong.knight@gmail.com>
2026-02-07 10:12:57 -0500

    add dap for python

---
+++ lua/plugins/debugging.lua
@@ -0,0 +1,33 @@
+return {
+	"mfussenegger/nvim-dap",
+	dependencies = {
+		"nvim-neotest/nvim-nio",
+		"rcarriga/nvim-dap-ui",
+		"mfussenegger/nvim-dap-python",
+	},
+	config = function()
+		local dap = require("dap")
+		local dapui = require("dapui")
+
+		require("dapui").setup()
+		require("dap-python").setup("uv")
+
+		dap.listeners.before.attach.dapui_config = function()
+			dapui.open()
+		end
+		dap.listeners.before.launch.dapui_config = function()
+			dapui.open()
+		end
+		dap.listeners.before.event_terminated.dapui_config = function()
+			dapui.close()
+		end
+		dap.listeners.before.event_exited.dapui_config = function()
+			dapui.close()
+		end
+		vim.keymap.set("n", "<Leader>dov", dap.step_over, {})
+		vim.keymap.set("n", "<Leader>dsi", dap.step_into, {})
+		vim.keymap.set("n", "<Leader>dot", dap.step_out, {})
+		vim.keymap.set("n", "<Leader>dt", dap.toggle_breakpoint, {})
+		vim.keymap.set("n", "<Leader>dc", dap.continue, {})
+	end,
+}
