98ea2049dd2e058e0a886dc3768e451f1dd4aae8
Jinsu Hong <jinsuhong.knight@gmail.com>
2026-02-15 21:49:30 -0500

    update xclip

--- init.lua
+++ init.lua
@@ -17,7 +17,9 @@ vim.opt.rtp:prepend(lazypath)
 require("vim-options")
 require("lazy").setup("plugins")
 
-
+if vim.fn.executable("xclip") == 1 then
+    vim.opt.clipboard = "unnamedplus"
+end
 
 
 
