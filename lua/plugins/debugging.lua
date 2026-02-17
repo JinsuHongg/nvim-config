5654df505436af28cbc1e4dd21979a075c94d3d7
Jinsu Hong <jinsuhong.knight@gmail.com>
2026-02-16 23:02:08 -0500

    update debug keymaps

--- lua/plugins/debugging.lua
+++ lua/plugins/debugging.lua
@@ -24,10 +24,13 @@ return {
 		dap.listeners.before.event_exited.dapui_config = function()
 			dapui.close()
 		end
-		vim.keymap.set("n", "<Leader>dov", dap.step_over, {})
-		vim.keymap.set("n", "<Leader>dsi", dap.step_into, {})
-		vim.keymap.set("n", "<Leader>dot", dap.step_out, {})
-		vim.keymap.set("n", "<Leader>dt", dap.toggle_breakpoint, {})
-		vim.keymap.set("n", "<Leader>dc", dap.continue, {})
+		vim.keymap.set("n", "<F5>", dap.continue)
+		vim.keymap.set("n", "<F10>", dap.step_over)
+		vim.keymap.set("n", "<F11>", dap.step_into)
+		vim.keymap.set("n", "<F12>", dap.step_out)
+		vim.keymap.set("n", "<Leader>b", dap.toggle_breakpoint)
+		vim.keymap.set("n", "<Leader>B", function()
+			dap.set_breakpoint(vim.fn.input("Breakpoint condition: "))
+		end)
 	end,
 }
