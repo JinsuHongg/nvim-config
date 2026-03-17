8d2a4aea84c748ba63eaa6d945d9187859707047
Jinsu Hong <jinsuhong.knight@gmail.com>
2026-03-17 00:45:18 -0400

    update clipboard setting

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -2,5 +2,6 @@ return {
 	"nvim-neo-tree/neo-tree.nvim",
 	keys = {
 		{ "<C-n>", "<cmd>Neotree toggle<cr>", desc = "Toggle Neo-tree" },
+		{ "<leader>e", "<cmd>Neotree focus<cr>", desc = "Focus Neo-tree" },
 	},
 }
