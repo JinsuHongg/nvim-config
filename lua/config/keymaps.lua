4129bf2609703e9523a1f3ee980cefec8dd91829
Jinsu Hong <jinsuhong.knight@gmail.com>
2026-04-13 21:59:39 -0400

    update configs

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,4 +1,6 @@
 -- bufferline
 vim.keymap.set("n", "<leader>bd", ":bp | bd #<CR>", { silent = true })
-
+vim.keymap.set("n", "<leader>dq", function()
+	vim.diagnostic.setqflist()
+end, { desc = "Diagnostics → quickfix" })
 -- neotree
